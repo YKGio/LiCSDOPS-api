7ff9e9b969934ae2aca816ab6e3382cc70595069
YKGio <aresr417@gmail.com>
2023-05-17 16:15:07 +0800

    project rename

--- coughmusicapi/wsgi.py
+++ coughmusicapi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for coughmusicapi project.
+WSGI config for COUGHit-api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coughmusicapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'COUGHit-api.settings')
 
 application = get_wsgi_application()

1795eca3e1d9f7d106ad70fc37582d5085bba790
YKGio <aresr417@gmail.com>
2023-05-23 18:18:50 +0800

    config update

--- api/lib/classes.py
+++ api/lib/classes.py
@@ -1,12 +1,3 @@
-import api.lib.obj.music as music
-import api.lib.obj.melody as melody
-import api.lib.obj.drum as drum
-import yaml
-
-class Config:
-    def __init__(self):
-        self.params = self.__load_config()
-
-    def __load_config(self):
-        with open('api/lib/config.yaml', 'r') as f:
-            return yaml.load(f, Loader=yaml.FullLoader)
+from api.lib.obj.melody import Melody
+from api.lib.obj.drum import Drum
+from api.lib.obj.music import Music
---
+++ api/lib/config.py
@@ -0,0 +1,63 @@
+import os
+import sys
+
+### PATH SETTINGS ###
+COUGHS_DIR = 'medias/audios/coughs'
+MIDIS_DIR = 'medias/midis'
+OUTPUT_DIR = 'medias/audios/output'
+
+
+
+API_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(API_DIR)
+sys.path.append(PROJECT_DIR)
+COUGHS_DIR = os.path.join(API_DIR, COUGHS_DIR)
+MIDIS_DIR = os.path.join(API_DIR, MIDIS_DIR)
+OUTPUT_DIR = os.path.join(API_DIR, OUTPUT_DIR)
+
+
+PITCH = 'PITCH'
+VOLUMN = 'VOLUMN'
+FADEOUT = 'FADEOUT'
+FILTER = 'FILTER'
+
+KICK = 'KICK'
+SNARE = 'SNARE'
+HIHAT = 'HIHAT'
+TOM1 = 'TOM1'
+TOM2 = 'TOM2'
+
+DRUM_MAP = {36:KICK, 38:SNARE, 42:HIHAT, 45:TOM1, 46:HIHAT, 48: TOM2, 50:TOM2, 51:HIHAT}
+
+DRUMSET = {
+    KICK : {
+        PITCH : 40,
+        VOLUMN : 0,
+        FADEOUT : 50,
+        FILTER : False
+    },
+    SNARE : {
+        PITCH : 80,
+        VOLUMN : 0,
+        FADEOUT : 50,
+        FILTER : False
+    },
+    HIHAT : {
+        PITCH : 60,
+        VOLUMN : -3,
+        FADEOUT : 20,
+        FILTER : True
+    },
+    TOM1 : {
+        PITCH : 45,
+        VOLUMN : -3,
+        FADEOUT : 50,
+        FILTER : False
+    },
+    TOM2 : {
+        PITCH : 50,
+        VOLUMN : -3,
+        FADEOUT : 50,
+        FILTER : False
+    },
+}
